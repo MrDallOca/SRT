49398066829a5bd6fae2d9620db2df40daac0778
Tiago Dall'Oca <tdalloca@gmail.com>
2016-11-25 09:39:18 -0200

    Algumas funcoes adicionais

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,7 +1,9 @@
-int IN1 = 4;
-int IN2 = 5;
-int IN3 = 6;
-int IN4 = 7;
+#define inv(n) { (n) *= -1; }
+
+#define IN1 4
+#define IN2 5
+#define IN3 6
+#define IN4 7
   
 void setup(){
   //Define os pinos como saida
@@ -9,40 +11,112 @@ void setup(){
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+  pinMode(13, OUTPUT);
 }
-  
+
+void rotacaoParaFrente(int q){
+  // gira para esquerda?
+  if (q < 0){
+    inv(q)
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+
+  // então gira para direita
+  else if(q > 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  else{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  while(q){
+    delay(10);
+    q--;
+  }
+}
+
+void rotacaoParaTras(int q){
+  // gira para esquerda?
+  if (q < 0){
+    inv(q)
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+
+  // então gira para direita
+  else if(q > 0){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  else{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  while(q){
+    delay(10);
+    q--;
+  }
+}
+
+void movimentacao(int q)
+{
+  // move para frente?
+  if (q > 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+  }
+
+  // move para tras?
+  else if (q < 0){
+    inv(q)
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+  }
+
+  // fica parado
+  else{
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+  }
+
+  while(q){
+    delay(10);
+    q--;
+  }
+}
+
+
 void loop(){
-  //Gira o Motor A no sentido horario
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  delay(2000);
-  //Para o motor A
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  delay(500);
-  //Gira o Motor B no sentido horario
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  delay(2000);
-  //Para o motor B
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
-  delay(500);
- 
-  //Gira o Motor A no sentido anti-horario
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, HIGH);
-  delay(2000);
-  //Para o motor A
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, HIGH);
-  delay(500);
-  //Gira o Motor B no sentido anti-horario
-  digitalWrite(IN3, LOW);
-  digitalWrite(IN4, HIGH);
-  delay(2000);
-  //Para o motor B
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, HIGH);
-  delay(500);
+  rotacaoParaFrente(400);
+  rotacaoParaFrente(-400);
+  
+  rotacaoParaTras(400);
+  rotacaoParaTras(-400);
+  
+  movimentacao(400);
+  movimentacao(-400);
 }
